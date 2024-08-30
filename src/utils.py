7a5be2cdbdf16e3e6ae26628166bfdfbdcef0a47
WaqiMallick <waqimalick123@gmail.com>
2024-08-30 13:37:03 +0500

    predicttion piepline and app

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-
+import pickle
 from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
@@ -44,3 +44,10 @@ def evaluate_model(x_train,y_train,x_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e,sys)     
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)    
